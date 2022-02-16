137145cafe60624a3620d3405af7837b35532954
MatiasEGonzalez <matiasgonzalez110@gmail.com>
2022-02-16 12:12:46 -0300

    Cuarta entrega

--- js/entregableCuatro.js
+++ js/entregableCuatro.js
@@ -1,30 +1,26 @@
+function calculadora(moneda, precioSinInt) {
 
-function calculadora(moneda, precioSinInt){
-
-    
     let impuestopais = 0.35;
     let impuestoCompra = 0.30;
     let precioConInt = (precioSinInt + (precioSinInt * (impuestopais + impuestoCompra)));
-    
-    
+
     alert("El precio oficial actual del " + moneda + " es: $" + precioConInt);
-    
+
+}
+
+
+
+for (let i = 0; i <= 10; i++) {
+    alert("Bienvenido a la calculadora de impuestos a la moneda extranjera");
+    let calcular = prompt("Desea calcular impuestos?\nsi o no");
+
+    if (calcular == "no") {
+        break;
+    } else if (calcular == "si") {
+        let moneda = prompt("Ingrese que moneda desea calcular");
+        let precioSinInt = parseInt(prompt("Ingrese el precio actual del " + moneda));
+        calculadora(moneda, precioSinInt);
+    } else {
+        alert("Ingrese una opcion valida");
     }
-    
-    
-    
-    for (let i = 1; i <= 10; i++) {
-        alert("Bienvenido a la calculadora de impuestos a la moneda extranjera")
-        let calcular = prompt("Desea calcular impuestos?\nsi o no");
-      
-        if(calcular == "no"){
-            break;
-        }
-        else if (calcular == "si"){
-            let moneda = prompt("Ingrese que moneda desea calcular");
-            let precioSinInt = parseInt(prompt("Ingrese el precio actual del " + moneda));
-            calculadora(moneda, precioSinInt);
-        }else{
-            alert("Ingrese una opcion valida")
-        }
-    }
+}
