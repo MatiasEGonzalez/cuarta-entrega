5c97d1402d230246ca75ef330f42b826b8a3560a
MatiasEGonzalez <matiasgonzalez110@gmail.com>
2022-02-16 11:50:23 -0300

    cuarta entrega

--- js/entregableCuatro.js
+++ js/entregableCuatro.js
@@ -1,30 +1,30 @@
-alert("Bienvenido a la calculadora de impuestos a la moneda extranjera")
 
-function calculadora(precioSinInt, PrecioConInt){
+function calculadora(moneda, precioSinInt){
 
-
-let moneda = prompt("Ingrese que moneda desea calcular");
-precioSinInt = parseInt(prompt("Ingrese el precio actual del " + moneda));
-let impuestopais = 0.35;
-let impuestoCompra = 0.30;
-PrecioConInt = (precioSinInt + (precioSinInt * (impuestopais + impuestoCompra)));
-
-
-alert("El precio oficial actual del " + moneda + " es: $" + PrecioConInt);
-
-}
-
-
-
-for (let i = 1; i <= 10; i++) {
-    let calcular = prompt("Desea calcular impuestos?\nSi o No");
-
-    if(calcular == "no"){
-        break;
+    
+    let impuestopais = 0.35;
+    let impuestoCompra = 0.30;
+    let precioConInt = (precioSinInt + (precioSinInt * (impuestopais + impuestoCompra)));
+    
+    
+    alert("El precio oficial actual del " + moneda + " es: $" + precioConInt);
+    
     }
-    else if (calcular == "si"){
-        calculadora();
-    }else{
-        alert("Ingrese una opcion valida")
+    
+    
+    
+    for (let i = 1; i <= 10; i++) {
+        alert("Bienvenido a la calculadora de impuestos a la moneda extranjera")
+        let calcular = prompt("Desea calcular impuestos?\nsi o no");
+      
+        if(calcular == "no"){
+            break;
+        }
+        else if (calcular == "si"){
+            let moneda = prompt("Ingrese que moneda desea calcular");
+            let precioSinInt = parseInt(prompt("Ingrese el precio actual del " + moneda));
+            calculadora(moneda, precioSinInt);
+        }else{
+            alert("Ingrese una opcion valida")
+        }
     }
-}
